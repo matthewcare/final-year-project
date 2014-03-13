ab68bb6c7d5b103b44fff4e281073f67c165103f
mattycare <matty_care@hotmail.co.uk>
2014-03-13 11:37:15 +0000

    Fluffed up the F major scale somehow
    F major had the wrong notes in the scale

--- js/scales.js
+++ js/scales.js
@@ -7,10 +7,10 @@ var scales = {
         CMAJOR:                 ["C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G", "A", "B", "C", "B", "A", "G", "F", "E", "D", "C", "B", "A", "G", "F", "E", "D", "C"],
         GMAJOR:                 ["G", "A", "B", "C", "D", "E", "F#", "G", "A", "B", "C", "D", "E", "F#", "G", "F#", "E", "D", "C", "B", "A", "G", "F#", "E", "D", "C", "B", "A", "G"],
         DMAJOR:                 ["D", "E", "F#", "G", "A", "B", "C#", "D", "E", "F#", "G", "A", "B", "C#", "D", "C#", "B", "A", "G", "F#", "E", "D", "C#", "B", "A", "G", "F#", "E", "D"],
-        FMAJOR:                 ["F", "G#", "A#", "B#", "C#", "D#", "E#", "F", "G#", "A#", "B#", "C#", "D#", "E#", "F", "E#", "D#", "C#", "B#", "A#", "G#", "F", "E#", "D#", "C#", "B#", "A#", "G#", "F"],
+        FMAJOR:                 ["F", "G", "A", "Bb", "C", "D", "E", "F", "G", "A", "Bb", "C", "D", "E", "F", "E", "D", "C", "Bb", "A", "G", "F", "E", "D", "C", "Bb", "A", "G", "F", "won't work until I work out Bb / A# problem"],
 
         AMINORHARMONIC:         ["A", "B", "C", "D", "E", "F", "G#", "A", "B", "C", "D", "E", "F", "G#", "A", "G#", "F", "E", "D", "C", "B", "A", "G#", "F", "E", "D", "C", "B", "A"],
-        DMINORHARMONIC:         ["D", "E", "F", "G", "A", "B", "C#", "D", "E", "F", "G", "A", "B", "C#", "D", "C#", "B", "A", "G", "F", "E", "D", "C#", "B", "A", "G", "F", "E", "D"],
+        DMINORHARMONIC:         ["D", "E", "F", "G", "A", "B", "C#", "D", "E", "F", "G", "A", "B", "C#", "D", "C#", "B", "A", "G", "F", "E", "D", "C#", "B", "A", "G", "F", "E", "D",],
 
         CMAJORCONTRARYLEFT:     ["C",  "B", "A", "G", "F", "E", "D", "C", "D", "E", "F", "G", "A", "B", "C"],
         CMAJORCONTRARYRIGHT:    ["C", "D", "E", "F", "G", "A", "B", "C",  "B", "A", "G", "F", "E", "D", "C"],
